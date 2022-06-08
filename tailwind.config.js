6784e5fc1dbde6b9ddb19cb7baa66f6d6e779a8a
Hossain-Sajjad <sunshinems58.com>
2022-06-09 03:03:37 +0600

    project initialized

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    daisyui: {
+        themes: ["business"],
+    },
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+}
