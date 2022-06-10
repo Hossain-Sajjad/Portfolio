d9bfc91fc0ec4ebce0b6e96cb817910639462a50
Hossain-Sajjad <sunshinems58.com>
2022-06-11 03:39:29 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Navbar from './Pages/Shared/Navbar';
+import Footer from './Pages/Shared/Footer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
